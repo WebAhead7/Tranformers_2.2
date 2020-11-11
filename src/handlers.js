4d00b8dfd3bf1c9f6c6396a9e1a9ab8e9f27a775
Ammaryus <amaryousef@gmail.com>
2020-11-11 16:53:54 +0200

    redirections added...

--- src/handlers.js
+++ src/handlers.js
@@ -2,21 +2,44 @@ const url = require("url");
 const path = require("path");
 const fs = require("fs");
 const autocompleteBy = require(path.join(__dirname, "thirdParty/thirdparty"));
+const serverError = '/public/serverError.html'
+function redirect(response, path) {
+  response.writeHead(302, { location: path });
+  response.end();
+}
 
 function serverErrorHandler(request, response) {
-  request.url = "/public/serverError.html";
-  resourcesHandler(request, response);
+  fs.readFile(
+    path.join(__dirname, "..", request.url + ".html"),
+    (err, file) => {
+      if (err) {
+        console.log(err);
+      } else {
+        response.writeHead(500, { "content-type": "text/html" });
+        response.end(file);
+      }
+    }
+  );
 }
 
 function notFoundHandler(request, response) {
-  request.url = "/public/notFound.html";
-  resourcesHandler(request, response);
+  fs.readFile(
+    path.join(__dirname, "..", request.url + ".html"),
+    (err, file) => {
+      if (err) {
+        console.log(err);
+      } else {
+        response.writeHead(404, { "content-type": "text/html" });
+        response.end(file);
+      }
+    }
+  );
 }
 
 function homeHandler(request, response) {
   fs.readFile(path.join(__dirname, "..", "public/index.html"), (err, file) => {
     if (err) {
-      serverErrorHandler(request, response);
+      redirect(response, "public/serverError");
     } else {
       response.writeHead(200, { "content-type": "text/html" });
       response.end(file);
@@ -34,7 +57,7 @@ function resourcesHandler(request, response) {
   if (types[contentType]) {
     fs.readFile(path.join(__dirname, "..", request.url), (err, file) => {
       if (err) {
-        serverErrorHandler(request, response);
+        redirect(response, "/public/serverError");
       } else {
         response.writeHead(200, { "contect-type": contentType });
         response.end(file);
@@ -65,9 +88,13 @@ function router(request, response) {
   } else if (path.startsWith("/autocomplete?")) {
     atucompleteHandler(request, response);
     response.end(); // delete this when atucomplete finished!!!!!!!!
+  } else if (pathname === "/public/notFound") {
+    notFoundHandler(request, response);
+  } else if (pathname === "/public/serverError") {
+    serverErrorHandler(request, response);
   } else {
     console.log(pathname);
-    notFoundHandler(request, response);
+    redirect(response, "public/notFound");
   }
 }
 
